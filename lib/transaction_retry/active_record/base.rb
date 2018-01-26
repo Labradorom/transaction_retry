e40b46d87f0115d25560a84aae6c6fc69d1b36af
Vlad Shvedov <init13@gmail.com>
2018-01-26 22:32:40 +0200

    A new way of exception capturing

--- lib/transaction_retry/active_record/base.rb
+++ lib/transaction_retry/active_record/base.rb
@@ -21,15 +21,18 @@ module TransactionRetry
 
           begin
             transaction_without_retry(*objects, &block)
-          rescue ::ActiveRecord::TransactionIsolationConflict
-            raise if retry_count >= TransactionRetry.max_retries
-            raise if tr_in_nested_transaction?
-            
-            retry_count += 1
-            postfix = { 1 => 'st', 2 => 'nd', 3 => 'rd' }[retry_count] || 'th'
-            logger.warn "Transaction isolation conflict detected. Retrying for the #{retry_count}-#{postfix} time..." if logger
-            tr_exponential_pause( retry_count )
-            retry
+          rescue ActiveRecord::StatementInvalid => ex
+            if ex.message == 'Deadlock found when trying to get lock; try restarting transaction'
+              raise if retry_count >= TransactionRetry.max_retries
+              raise if tr_in_nested_transaction?
+              
+              retry_count += 1
+              postfix = { 1 => 'st', 2 => 'nd', 3 => 'rd' }[retry_count] || 'th'
+              logger.warn "Transaction isolation conflict detected. Retrying for the #{retry_count}-#{postfix} time..." if logger
+              tr_exponential_pause( retry_count )
+
+              retry
+            end
           end
         end
         
